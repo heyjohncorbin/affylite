cca0088f8598acab26672c6f384b2a52a11c0c14
John Corbin <58629912+heyjohncorbin@users.noreply.github.com>
2020-07-21 18:13:12 -0400

    Add files via upload

---
+++ affylite/affylite.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Plugin Name: Affylite
+ * Plugin URI:  https://md-wv.com
+ * Description: Add a simple affiliate disclaimer to the top of each post. Easy, lite, and no bloat. Under 5kb.
+ * Version:     1.0
+ * Author:      MDWV
+ * Author URI:  https://heyjohncorbin.com
+ * License:     GPLv2 or later.
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: affylite
+*/
+
+// Admin page to set a custom affiliate disclaimer
+add_action( 'admin_menu', 'affylite_add_admin_menu' );
+add_action( 'admin_init', 'affylite_settings_init' );
+
+	// Admin page settings
+	function affylite_add_admin_menu(  ) { 
+		add_menu_page( 'Affylite', 'Affylite', 'manage_options', 'affylite', 'affylite_options_page', 'dashicons-format-chat');
+	}
+
+	// Admin page section descriptions
+	function affylite_settings_init(  ) { 
+		register_setting( 'pluginPage', 'affylite_settings' );
+		add_settings_section(
+			'affylite_pluginPage_section', 
+			__( 'Add a simple affiliate disclaimer to the top of each post.', 'affylite' ), 
+			'affylite_settings_section_callback', 
+			'pluginPage'
+		);
+		add_settings_field( 
+			'affylite_custom_disclaimer', 
+			__( 'Affylite supports basic HTML.', 'affylite' ), 
+			'affylite_custom_disclaimer_render', 
+			'pluginPage', 
+			'affylite_pluginPage_section' 
+		);
+	}
+
+	// Admin page custom textarea
+	function affylite_custom_disclaimer_render(  ) { 
+		$affylite_options = get_option( 'affylite_settings' );
+		?>
+		<p>
+			<textarea cols='50' rows='10' name='affylite_settings[affylite_custom_disclaimer]'><?php echo $affylite_options['affylite_custom_disclaimer']; ?></textarea>
+		</p>
+		<?php
+	}
+
+	// Admin page render
+	function affylite_options_page(  ) { 
+			?>
+			<form action='options.php' method='post'>
+				<h2>Affylite - Easy Affiliate Disclaimer</h2>
+				<?php
+				settings_fields( 'pluginPage' );
+				do_settings_sections( 'pluginPage' );
+				submit_button();
+				?>
+			</form>
+			<?php
+	}
+
+// Append post text
+function affylite_filter( $original ) {
+	$affylite_options = get_option( 'affylite_settings' );
+	if ( $affylite_options['affylite_custom_disclaimer'] != '' ) {
+		if ( is_singular('post') ) {
+			$affylite_disclaimer = '<p><em>' . $affylite_options['affylite_custom_disclaimer'] . '</em></p>';
+			$affylite_disclaimer .= $original;
+			return $affylite_disclaimer;
+		}
+		else {
+			return $original;
+		}
+	}
+	else {
+		if ( is_singular('post') ) {
+			$affylite_disclaimer = '<p><em>We use affiliate links. If you purchase something using one of these links, we may receive compensation or commission.</em></p>';
+			$affylite_disclaimer .= $original;
+			return $affylite_disclaimer;
+		}
+		else {
+			return $original;
+		}
+	}
+}
+
+add_filter( 'the_content', 'affylite_filter' );
---
+++ affylite/readme.txt
@@ -0,0 +1,14 @@
+=== Affylite ===
+Contributors: John Corbin, MDWV
+Tags: affiliate, disclaimer, affylite, lite
+Requires at least: 5.0
+Tested up to: 5.4.2
+Requires PHP: 5.6
+License: GPLv2 or later.
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+
+Affylite - Easy Affiliate Disclaimer
+
+== Description ==
+Add a simple affiliate disclaimer to the top of each post. Easy, lite, and no bloat. Under 5kb.
+Sample disclosure is available - set your own custom message using the admin page. Use at your own risk/reward - seek counsel for appropriate dislaimer message.
